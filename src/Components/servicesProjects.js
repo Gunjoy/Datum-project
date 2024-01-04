0d9af3372a7b1a07a5131245515d4c48a17e7c39
Gunjoy <151236456+Gunjoy@users.noreply.github.com>
2024-01-04 14:45:53 +0300

    commit 2

--- src/Components/servicesProjects.js
+++ src/Components/servicesProjects.js
@@ -3,90 +3,99 @@ import "./servicesProjcts.css";
 import { Button, Form, InputGroup, Table } from "react-bootstrap";
 import Dropdown from "react-bootstrap/Dropdown";
 
-function formInTable() {
-  return (
-    <div>
-      <Form>
-        <div className="TableInput">
-          <div>
-            <input />
-          </div>
-          <div>
-            <input />
-          </div>
-          <div>
-            <input />
-          </div>
-        </div>
-      </Form>
-    </div>
-  );
-}
 
-const startValue = {
-  nameProject: "",
-};
 export default function ServProj() {
-  const [arrInputs, setArrInputs] = useState([formInTable]);
+    const [oneService, setOneService] = useState({
+      nameOneServ: ""
+    })
+    const [arrService, setArrService] = useState([])
 
-  const [project, setProject] = useState(startValue);
-  const [projInTable, setProjInTable] = useState([]);
+    const handleAddArrServ = (e) => {
+      setArrService((prevstate) => [...prevstate,oneService])
+      setOneService({
+        nameOneServ: ""
+      })
+    }
+    console.log("МАССИВ УСЛУГ ПОПОЛНИЛСЯ", arrService)
+  
+   const [projectServ, setProjService] = useState({
+    nameProject: "" 
+  })
+  const [addedProjectServ, setAddedProjectServ] = useState([])
 
-  const handleSubmitProject = (e) => {
-    e.preventDefault();
-    setProjInTable((prevState) => [...prevState, project]);
-    setProject(startValue);
-  };
-  console.log("Project_IN_TABLE:", projInTable);
-  console.log("project", project);
-  // console.log("projcetHUI:", project);
+  const handleSubmitProjectServ = (e) => {
+      e.preventDefault();
+      setAddedProjectServ((prevState) => [...prevState, projectServ])
+      setProjService({
+        nameProject: ""
+      })
+  }
+  //  console.log("Добавляемый проект" ,addedProjectServ)
+      console.log("Добовляемая услуга", oneService)
+  
   return (
     <div>
       <div>
-        <h1 className="titleServisesProjects">Услуги Проектов</h1>
-        <div>
+        <Form onSubmit={handleSubmitProjectServ}>
           <div>
-            <Form onSubmit={handleSubmitProject}>
-              <div>
-                <Button type="submit" className="creatServProjets">
-                  Добавить услуги проекта
-                </Button>
-              </div>
-              <input
-                placeholder="Ведите имя проетка"
-                onChange={(e) =>
-                  setProject((prevState) => ({
-                    ...prevState,
-                    nameProject: e.target.value,
-                  }))
-                }
-                value={project.nameProject}
-              />
-            </Form>
+            <div>
+              <input placeholder="Ведите имя проетка" onChange={(e) =>{
+                setProjService((prevState) => ({
+                  ...prevState,
+                    nameProject: e.target.value
+                }))
+              }}
+              value={projectServ.nameProject}
+                >
+
+              </input>
+            </div>
+            <div>
+              <Button type="reset" >Отчистить</Button>  
+              <Button type="submit" >Добавить</Button>
+            </div>
           </div>
-          <Table className="tableServises">
-            <thead>
-              <tr>
-                <th>Проект</th>
-                <th>Добавление услгуи</th>
-                <th>Услуги</th>
-              </tr>
-            </thead>
-            <tbody>
-              {projInTable.map((proj, index) => {
-                return (
-                  <tr>
-                    <td>{proj.nameProject}</td>
-                    <td></td>
-                    <td>
-                      <h1></h1>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </div>
+        </Form>
+      </div>
+      <div>
+        <Table>
+          <thead>
+            <tr>
+              <th>Проект</th>
+              <th>Добавление услгуи</th>
+              <th>Услуги</th>
+            </tr>
+          </thead>
+          <tbody>
+            {addedProjectServ.map((elem, index) => {
+              
+              return(
+                <tr>
+                  <td>{elem.nameProject}</td>
+                  <td>
+                    <input placeholder="Имя услуги" onChange={(e) => {
+                      setOneService((prevstate) => ({
+                        ...prevstate,
+                        nameOneServ: e.target.value
+                      }))
+                    }} 
+                    value={oneService.nameOneServ}/>
+                    <Button onClick={handleAddArrServ}>Добавить</Button>
+                  </td>
+                  <td>{arrService.map((elemen, index) => {
+                    return(
+                      <div className="FirstDivForArrServ">
+                        <div className="SecDivForArrServ">
+                          <h3>{elemen.nameOneServ}, {index}</h3>
+                        </div>
+                      </div>
+                    )
+                  })}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
       </div>
     </div>
   );
